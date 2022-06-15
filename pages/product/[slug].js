6029300931d7f1b449e2c9ed7c668456bd77cb48
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2022-06-14 23:32:59 -0400

    product page updated with may-like carousel

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -80,9 +80,20 @@ const ProductDetails = ({ product, products }) => {
             >
               Add to Cart
             </button>
-            <button type="button" className="buy-now">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
